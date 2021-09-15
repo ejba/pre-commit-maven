4b4e1a889ede5f81a1cf72ebac72c10c4efd510c
Emanuel Alves <emanuel.j.b.alves@gmail.com>
2021-09-15 21:01:28 +0100

    Fixing #5 by providing a default value to the main function

--- pre_commit_maven/maven.py
+++ pre_commit_maven/maven.py
@@ -1,12 +1,19 @@
 import argparse
 import os.path
+import sys
+from typing import Optional, Sequence
 from pre_commit_maven.utils import maven
 from pre_commit_maven.utils import generic_main
 
 CWD = os.getcwd()
 
 
-def main(argv: list, cwd=CWD, print_fn=print, execute_fn=generic_main.execute) -> int:
+def main(
+    argv: Optional[Sequence[str]] = None,
+    cwd=CWD,
+    print_fn=print,
+    execute_fn=generic_main.execute,
+) -> int:
     parser = argparse.ArgumentParser()
     parser.add_argument("goals", nargs="*", help="maven goals to run")
     args = parser.parse_args(argv)
